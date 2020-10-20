69f8c0d0138d770c0f1c1df0fe8017a61b42d2bb
Jesus Cristian Aleman Fuentes <jesus.aleman@globant.com>
2020-10-20 09:40:55 -0500

    configuring 2 screens

--- App.js
+++ App.js
@@ -11,6 +11,8 @@ import {
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import Inicio from './views/Inicio';
+import Nosotros from './views/Nosotros';
 
 const Stack = createStackNavigator();
 
@@ -18,7 +20,10 @@ const App: () => React$Node = () => {
   return (
     <>
       <NavigationContainer>
-        <Stack.Navigator></Stack.Navigator>
+        <Stack.Navigator initialRouteName="Inicio">
+          <Stack.Screen name="Inicio" component={Inicio} />
+          <Stack.Screen name="Nosotros" component={Nosotros} />
+        </Stack.Navigator>
       </NavigationContainer>
     </>
   );

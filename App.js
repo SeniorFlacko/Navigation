4e44842a433d41339f4a87c1cd9b5707ee669ee6
Jesus Cristian Aleman Fuentes <jesus.aleman@globant.com>
2020-10-20 09:36:10 -0500

    first commit with react navigation included

---
+++ App.js
@@ -0,0 +1,29 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <NavigationContainer>
+        <Stack.Navigator></Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
---
+++ views/Nosotros.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+const Nosotros = (props) => {
+  return (
+    <View>
+      <Text>Nosotos works!!</Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({});
+export default Nosotros;

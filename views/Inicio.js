3a8ff29d76da0cd90b619335ea55112b12166a07
Jesus Cristian Aleman Fuentes <jesus.aleman@globant.com>
2020-10-20 09:57:30 -0500

    passing data between screens

--- views/Inicio.js
+++ views/Inicio.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import {View, StyleSheet, Text, Button} from 'react-native';
-const Nosotros = (props) => {
+
+const Inicio = (props) => {
+  const informacion = {
+    clienteId: 20,
+    totalpagar: 500,
+  };
+
   return (
     <View style={styles.contenedor}>
       <Text>Inicio works!!</Text>
       <Button
         title="Ir a Nosotros"
         onPress={() => {
-          props.navigation.navigate('Nosotros');
+          props.navigation.navigate('Nosotros', informacion);
         }}
       />
     </View>
@@ -20,4 +26,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
-export default Nosotros;
+export default Inicio;
--- views/Nosotros.js
+++ views/Nosotros.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import {View, StyleSheet, Text, Button} from 'react-native';
+
 const Nosotros = (props) => {
+  const {clienteId, totalpagar} = props.route.params;
   return (
     <View style={styles.contenedor}>
-      <Text>Nosotos works!!</Text>
+      <Text>Cliente Id: </Text>
+      <Text>{clienteId}</Text>
+      <Text>Total a Pagar: </Text>
+      <Text>{totalpagar}</Text>
       <Button
         title="Volver"
         onPress={() => {
